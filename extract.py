5bf2e99e7c37509b3efdc5b588501ae0cba9822f
braulionova <braulio503@gmail.com>
2023-02-08 18:01:51 -0400

    add empresa return

--- extract.py
+++ extract.py
@@ -25,3 +25,33 @@ def doBackgroundTask(inp):
     print("Doing background task")
     print(inp.msg)
     print("Done")
+    
+def getRNC(driver: webdriver.Chrome) -> str:
+    #buscamos el rnc
+    driver.get("https://www.dgii.gov.do/app/WebApps/ConsultasWeb/consultas/rnc.aspx")
+
+    # Fill in the text field
+    text_field = driver.find_element(By.ID, 'ctl00_cphMain_txtRNCCedula')
+    text_field.send_keys("101013702")
+
+    # Click the button
+    button = driver.find_element(By.ID, 'ctl00_cphMain_btnBuscarPorRNC')
+    button.click()
+
+    # Wait for the results page to load
+    driver.implicitly_wait(10)
+
+    # Get the results page title
+    results_page_title = driver.title
+
+    # Print the results page title
+    print("Results page title:", results_page_title)
+
+    #RNC
+    rnc_empresa = driver.find_element(By.XPATH, '//*[@id="ctl00_cphMain_dvDatosContribuyentes"]/tbody/tr[1]/td[2]')
+
+    print(rnc_empresa.text)
+    #nombre empresa
+    nombre_empresa = driver.find_element(By.XPATH, '//*[@id="ctl00_cphMain_dvDatosContribuyentes"]/tbody/tr[2]/td[2]')
+    
+    return nombre_empresa
